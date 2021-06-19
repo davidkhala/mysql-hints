8ca930d86b251034823771d1bb16422af3d1a164
David Liu <david.yx.liu@oracle.com>
2021-06-19 09:34:42 +0800

    add setup.sh#config

--- router/setup.sh
+++ router/setup.sh
@@ -1,23 +1,24 @@
 set -e
-install(){
+install() {
   sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
   sudo yum -y install mysql-router
 }
-config(){
+config() {
   local privateEndpoint=$1
-  
-  tee -a /etc/mysqlrouter/mysqlrouter.conf << END
+
+  sudo tee -a /etc/mysqlrouter/mysqlrouter.conf << EOF
   [routing:redirect_classic]
   bind_address = 0.0.0.0:3306
   destinations = ${privateEndpoint}:3306
   routing_strategy=first-available
- 
+
   [routing:redirect_xprotocol]
   bind_address = 0.0.0.0:33060
   destinations = ${privateEndpoint}:33060
   protocol = x
   routing_strategy=first-available
-  END
+  
+EOF
 
 }
 $@
