7ede6deb6e85b921c866931b4c39c0fa45343fe1
David Liu <david.yx.liu@oracle.com>
2021-06-19 09:39:28 +0800

    add setup.sh#service, firewall

--- router/setup.sh
+++ router/setup.sh
@@ -6,7 +6,7 @@ install() {
 config() {
   local privateEndpoint=$1
 
-  sudo tee -a /etc/mysqlrouter/mysqlrouter.conf << EOF
+  sudo tee -a /etc/mysqlrouter/mysqlrouter.conf <<EOF
   [routing:redirect_classic]
   bind_address = 0.0.0.0:3306
   destinations = ${privateEndpoint}:3306
@@ -17,8 +17,19 @@ config() {
   destinations = ${privateEndpoint}:33060
   protocol = x
   routing_strategy=first-available
-  
+
 EOF
 
+}
+service() {
+  sudo systemctl start mysqlrouter.service
+  sudo systemctl status mysqlrouter.service
+  sudo systemctl enable mysqlrouter.service
+}
+firewall() {
+  sudo firewall-cmd --permanent --add-port=3306/tcp
+  sudo firewall-cmd --permanent --add-port=33060/tcp
+  sudo firewall-cmd --reload
+  sudo firewall-cmd --list-all
 }
 $@
