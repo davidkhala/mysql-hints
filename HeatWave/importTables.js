5ef25575e83b95345c41cd33c89f0ed963e4a3a8
David Liu <david.yx.liu@oracle.com>
2021-07-19 07:23:24 -0400

    Update importTables.js

--- HeatWave/importTables.js
+++ HeatWave/importTables.js
@@ -1,15 +1,15 @@
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Fnation.tbl", { schema:"tpch_1024", table:"NATION", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Fnation.tbl", { schema:"tpch_1024", table:"NATION", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
 
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Fcustomer.tbl", { schema:"tpch_1024", table:"CUSTOMER", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Fcustomer.tbl", { schema:"tpch_1024", table:"CUSTOMER", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
 
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Flineitem.tbl", { schema:"tpch_1024", table:"LINEITEM", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Flineitem.tbl", { schema:"tpch_1024", table:"LINEITEM", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
 
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Forders.tbl", { schema:"tpch_1024", table:"ORDERS", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1})
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Forders.tbl", { schema:"tpch_1024", table:"ORDERS", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1})
 
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Fpart.tbl", { schema:"tpch_1024", table:"PART", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Fpart.tbl", { schema:"tpch_1024", table:"PART", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
 
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Fpartsupp.tbl", { schema:"tpch_1024", table:"PARTSUPP", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Fpartsupp.tbl", { schema:"tpch_1024", table:"PARTSUPP", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
 
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Fregion.tbl", { schema:"tpch_1024", table:"REGION", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Fregion.tbl", { schema:"tpch_1024", table:"REGION", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1}) 
 
-util.importTable("https://objectstorage.us-ashburn-1.oraclecloud.com/n/idazzjlcjqzj/b/bucket-20201125-1020/o/tpch%2Fsupplier.tbl", { schema:"tpch_1024", table:"SUPPLIER", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1})
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/tpc-mysql-output%2Fsupplier.tbl", { schema:"tpch_1024", table:"SUPPLIER", fieldsTerminatedBy:"|", bytesPerChunk:"100M", threads:16, skipRows:1})
