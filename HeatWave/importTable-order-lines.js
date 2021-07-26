54f7c3954e418b449e41f5aafb729f8f8e5be8c4
David Liu <david.yx.liu@oracle.com>
2021-07-26 03:35:48 -0400

    Create importTable-order-lines.js

---
+++ HeatWave/importTable-order-lines.js
@@ -0,0 +1,1 @@
+util.importTable("https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/order-lines%2Fexport.csv", {schema: "tpch_1024", table: "SampleOrderLinesLarge", dialect: "csv-unix", skipRows: 1, showProgress: true})
