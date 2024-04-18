5ca4774f2c9c0577fdb5c5d55ee280d0adb5d135
David Liu <david-khala@hotmail.com>
2024-04-18 12:07:14 +0800

    Update user.sql

--- admin/user.sql
+++ admin/user.sql
@@ -1,6 +1,8 @@
 -- Create root@*
 CREATE USER 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+-- expose root
+ALTER USER 'root'@'%' IDENTIFIED BY 'password'
 
 -- List Users
 SELECT User, Host
